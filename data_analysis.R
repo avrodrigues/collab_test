799d6f738b385d4c790cd7ff5163b7f09cc52a7c
Arthur Rodrigues <rodrigues.arthur.v@gmail.com>
2024-09-19 14:02:32 +0300

    change the temp diversity relationship

--- data_analysis.R
+++ data_analysis.R
@@ -17,7 +17,7 @@ aridity <- runif(n, 0.1, 0.9) # Random aridity index values
 
 # Simulate diversity as a function of temperature, precipitation, and aridity
 # We assume a hypothetical relationship
-diversity <- 20 + 0.8 * temperature + rnorm(n, 0, 2) #- 0.05 * precipitation - 10 * aridity
+diversity <- 20 + 1.8 * temperature + rnorm(n, 0, 2) #- 0.05 * precipitation - 10 * aridity
 
 # Create a data frame
 data <- data.frame(temperature, precipitation, aridity, diversity)
@@ -48,3 +48,5 @@ print(p1)
 # Compare models using summary statistics
 summary(model_full)
 summary(model_temp)
+
+
