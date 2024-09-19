7b3873e3472ee0142dff4db66b3dd1bb28c1b19c
Natalie van Dis <natalie.vandis@helsinki.fi>
2024-09-19 13:52:27 +0300

    Correct spelling error

--- data_analysis.R
+++ data_analysis.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 library(dplyr)
 
-# the code simmulates a relattionship between diversity and temperature.
+# the code simulates a relattionship between diversity and temperature.
 # then it analyse it using temperature, precipitaion and aridity.
 # what model will be better, only with temperature, or with the three variables
 
